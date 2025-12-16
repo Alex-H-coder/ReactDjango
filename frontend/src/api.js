e480b7d40ee698c91506080fda07202fd9270a31
Alex-H-coder <alexhabonimana01@gmail.com>
2025-12-16 20:00:16 +0200

    worked on protected routes in react

---
+++ frontend/src/api.js
@@ -0,0 +1,21 @@
+//INTERCEPTORS FOR ADDING TOKEN TO REQUEST HEADERS.
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL  // Use VITE_API_BASE_URL from environment variables the .env file
+});
+
+api.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem('ACCESS_TOKEN'); // Retrieve the token from local storage
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}` //passing a jwt token
+        }
+        return config
+    },
+    (error) => {
+        return Promise.reject(error) // Handle request error
+    }
+)
+
+export default api; // Export the configured axios instancest ACCESS_TOKEN
