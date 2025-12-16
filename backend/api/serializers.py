45ff71d893f926300e05b84e35ed5f4fe92af43a
Alex-H-coder <alexhabonimana01@gmail.com>
2025-12-16 15:05:17 +0200

    Added react js frontend framwork

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
+from .models import *
+
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +13,11 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validate_data):
         user = User.objects.create_user(**validate_data)
         return user
+    
+class NoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Note
+        fields = ['id', 'title', 'content', 'created_at', 'author']
+        extra_kwargs = {'author':{'read_only': True}} #only the signed in user will be displayed. Users cannot define the author
+        
+        
